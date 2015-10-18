6122a2061b1224f4752c15e03a30117dfeeef52e
Vesihiisi <alicia@fagerving.se>
2015-10-18 13:12:59 +0200

    better randomizing function

--- getPics.php
+++ getPics.php
@@ -33,30 +33,55 @@ function getListOfTimesToLookUp($timestring)
     return $timesToLookUp;
 }
 
-function getRandomPicFromCategory($time) {
+function makeACategoryNameOutOfTime($time)
+{
+    return "Time_" . $time;
+}
+
+function getAllCategoryMembers($categoryName)
+{
     $base = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&format=json&cmtype=file&cmtitle=Category:";
-    $category = "Time_" . $time;
-    $url = $base . $category;
+    $url = $base . $categoryName;
     $res = json_decode(curl($url), true);
     $categorymembers = $res["query"]["categorymembers"];
-    if (count($categorymembers) > 0) {
-        $randomFile = $categorymembers[array_rand($categorymembers)];
-        $pageid = $randomFile["pageid"];
-        $title = str_replace(" ", "_", $randomFile["title"]);
-        $creditUrl = "https://commons.wikimedia.org/wiki/" . $title;
-        $thumbUrl = "https://commons.wikimedia.org/w/api.php?action=query&format=json&prop=imageinfo&iiprop=url&iiurlwidth=640&titles=" . $title;
-        $thumb = json_decode(curl($thumbUrl), true);
-        $result = array($creditUrl, $thumb["query"]["pages"][$pageid]["imageinfo"][0]["thumburl"]);
-        return json_encode($result);
+    return $categorymembers;
+}
+
+function getAllThePicturesForOneOrMoreTime($timesToLookUp)
+{
+    $allThePictures = array();
+    foreach ($timesToLookUp as $time) {
+        $categorymembers = getAllCategoryMembers(makeACategoryNameOutOfTime($time));
+        foreach ($categorymembers as $member) {
+            array_push($allThePictures, $member);
+        }
     }
+    return $allThePictures;
+}
+
+function getRandomArrayMember($array)
+{
+    if (count($array) == 1) {
+        return $array[0];
+    } else {
+        return $array[array_rand($array)];
+    }
+}
+
+function getRandomPic($arrayOfPics)
+{
+    $pic = getRandomArrayMember($arrayOfPics);
+    $pageid = $pic["pageid"];
+    $title = str_replace(" ", "_", $pic["title"]);
+    $creditUrl = "https://commons.wikimedia.org/wiki/" . $title;
+    $thumbUrl = "https://commons.wikimedia.org/w/api.php?action=query&format=json&prop=imageinfo&iiprop=url&iiurlwidth=640&titles=" . $title;
+    $thumb = json_decode(curl($thumbUrl), true);
+    $result = array($creditUrl, $thumb["query"]["pages"][$pageid]["imageinfo"][0]["thumburl"]);
+    return json_encode($result);
 }
 
 $time = $_POST["time"];
 
 $timesToLookUp = getListOfTimesToLookUp($time);
-$result = array();
-foreach ($timesToLookUp as $time) {
-    array_push($result, getRandomPicFromCategory($time));
-}
-
-echo $result[array_rand($result)];
+$allThePictures = getAllThePicturesForOneOrMoreTime($timesToLookUp);
+echo getRandomPic($allThePictures);
